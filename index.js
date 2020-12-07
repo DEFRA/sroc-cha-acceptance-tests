5b8bb8692fde48fff92604d0ed5f52a41ae126be
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2020-12-07 17:21:59 +0000

    Update project to use commander.js
    This includes some refactoring of the code to handle arguments out to a separate file to make things a clearer and easier to follow.

--- index.js
+++ index.js
@@ -1,35 +1,28 @@
 'use strict'
 
-const fs = require('fs')
+const { program } = require('commander')
+const ParamHelper = require('./lib/param_helper')
 const newman = require('newman')
 
-const environment = process.argv.slice(2)[0]
+program
+  .name('cha-tests')
+  .requiredOption('-e, --environment <environment>', 'environment to run tests against')
 
-if (!environment) {
-  console.log("You didn't say which environment file to use!")
-  process.exit(1)
-}
-
-const enviromentFile = fs.readdirSync('./environments', { withFileTypes: true })
-  .filter(item => !item.isDirectory())
-  .map(item => item.name)
-  .find(item => item.startsWith(environment))
-
-if (!enviromentFile) {
-  console.log(`Sorry, we can't find a matching '${environment}' environment file.`)
-  process.exit(1)
-}
-
-let collectionFile = 'cha.postman_collection.json'
-if (environment === 'example') {
-  collectionFile = 'ci.postman_collection.json'
-}
-
-newman.run({
-  collection: require(`./${collectionFile}`),
-  environment: require(`./environments/${enviromentFile}`),
-  reporters: 'cli'
-}, function (err) {
-  if (err) { throw err }
-  console.log('Collection run complete!')
+program.on('--help', () => {
+  console.log('')
+  console.log('Examples:')
+  console.log('  $ npm start -- -e dev')
+  console.log('  $ node index.js -e dev')
 })
+
+program.parse()
+
+const args = ParamHelper.params(program)
+
+newman.run(
+  args,
+  function (err) {
+    if (err) { throw err }
+    console.log('Collection run complete!')
+  }
+)
