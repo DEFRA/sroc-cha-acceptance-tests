74cb096b1f4ab4719aa1fa2815328b278c79da15
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2020-12-07 17:54:37 +0000

    Add support for reporters CLI option (#4)
    Now we have a proper command-line option parser we can add support for allowing users to specify which reporters to use.
    
    This change updates the project to
    
    - add the command line option
    - handle parsing the values provided and passing them to Newman

--- index.js
+++ index.js
@@ -7,12 +7,13 @@ const newman = require('newman')
 program
   .name('cha-tests')
   .requiredOption('-e, --environment <environment>', 'environment to run tests against')
+  .option('-r, --reporters [reporters...]', 'reporters you wish newman to use', 'cli')
 
 program.on('--help', () => {
   console.log('')
   console.log('Examples:')
   console.log('  $ npm start -- -e dev')
-  console.log('  $ node index.js -e dev')
+  console.log('  $ npm start -- -e dev -r cli,json')
 })
 
 program.parse()
